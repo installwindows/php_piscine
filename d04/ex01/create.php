5e31ba0e428ad42159ec6b7900bf589a1b131e81
Vincent Arnaud <varnaud@student.42.us.org>
2017-06-10 03:47:38 -0700

    ex01

--- d04/ex01/create.php
+++ d04/ex01/create.php
@@ -1,11 +1,30 @@
 <?php
-if ($_POST['submit'] !=== "OK" || !isset($_POST['login']) || $_POST['login'] === "" || !isset($_POST['passwd'] || $_POST['passwd'] === "")
+if ($_POST['submit'] !== "OK" || !isset($_POST['login']) || $_POST['login'] === "" || !isset($_POST['passwd']) || $_POST['passwd'] === "")
 {
 	echo "ERROR\n";
 	return ;
 }
 $hashed_pwd = hash('whirlpool', $_POST['passwd']);
 
-if (file_exists('private/passwd'))
+if (!file_exists('../private'))
 {
+	mkdir('../private');
+	file_put_contents("../private/passwd", "");
 }
+$file = file_get_contents("../private/passwd");
+if ($file !== "")
+{
+	$file = unserialize($file);
+	foreach ($file as $entry)
+	{
+		if ($entry['login'] === $_POST['login'])
+		{
+			echo "ERROR\n";
+			return ;
+		}
+	}
+}
+$file[] = ['login' => $_POST['login'], 'passwd' => $hashed_pwd];
+$file = serialize($file);
+file_put_contents("../private/passwd", $file);
+echo "OK\n";
